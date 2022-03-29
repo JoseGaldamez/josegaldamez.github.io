69411fe36829965a09e9e27f3955e5ed52f14f41
Jose Galdamez <josegaldamez1991@gmail.com>
2022-03-28 21:05:28 -0600

    Responsive banner contact

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6535737dd2672971"; }
+/******/ 		__webpack_require__.h = function() { return "e0832a6081dbbcca"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
